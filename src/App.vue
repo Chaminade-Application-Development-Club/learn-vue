3d06ad1db4ddd19ac0506fb2ca2416fe417b9a31
Leo428 <huzheyuan428@gmail.com>
2017-11-21 17:55:52 -0800

    First Commit from School Laptop

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app light>
+    <v-toolbar dark color="primary">
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title class="white--text">Title</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+        <v-icon>search</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+      </v-toolbar>
+
+    <v-navigation-drawer class="pink lighten-2" app absolute light overflow v-model="drawer">
+      <v-list>
+        <v-list-tile-avatar>
+          <img src="https://randomuser.me/api/portraits/men/85.jpg"/>
+        </v-list-tile-avatar>
+      </v-list>  
+    </v-navigation-drawer>
+    
+    
+    <v-content>
+      <v-container fluid>
+        <v-btn color = "primary"> 
+          Hello
+        </v-btn>
+      </v-container>
+    </v-content>
+
+    <v-footer :fixed="true" app>
+      <span>&copy; H2O</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
